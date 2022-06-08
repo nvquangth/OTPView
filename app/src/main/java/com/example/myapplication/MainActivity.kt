3c1164dafa31f2ce69affe27856aa682b09c72c1
quangnv <nguyenvanquang96th@gmail.com>
2022-06-08 23:56:58 +0700

    update otp view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.otpview.OTPListener
+import com.otpview.OtpTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val otpView = findViewById<OtpTextView>(R.id.otpView)
+        val buttonOk = findViewById<Button>(R.id.buttonOk)
+
+        otpView.otpListener = object : OTPListener {
+            override fun onInteractionListener() {
+                if((otpView.otp?.length ?: 0) < 6) {
+                    buttonOk.isEnabled = false
+                }
+            }
+
+            override fun onOTPComplete(otp: String) {
+                buttonOk.isEnabled = true
+                Toast.makeText(this@MainActivity, "otp = $otp", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
